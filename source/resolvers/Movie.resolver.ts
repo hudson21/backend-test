0228d10c8e074dd2a6f093e2cd7fd328cf9bd352
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-08-28 19:18:46 -0500

    Starting to work with Movie entity

---
+++ source/resolvers/Movie.resolver.ts
@@ -0,0 +1,35 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { ObjectId } from "mongodb";
+import { Ticket } from "../entities/ticket";
+
+import { AddTicketInput, ListTicketsInput, TicketInput, SaveTicketsInput, ListTicketsWithPagination } from "./types/Ticket.input";
+import { saveTickets, addTicket, ticket, listTickets, listTicketsWithPagination } from '../services/Ticket.service';
+
+@Resolver(() => Ticket)
+export class TicketResolver {
+
+  @Query(() => Ticket, { nullable: true })
+  public async ticket(@Arg("input") ticketInput: TicketInput): Promise<Ticket> {
+    return ticket(ticketInput);
+  }
+
+  @Query(() => [Ticket])
+  public async listTickets(@Arg("input") ticketInput: ListTicketsInput): Promise<Ticket[]> {
+    return listTickets(ticketInput);
+  }
+
+  @Query(() => [Ticket])
+  public async listTicketsWithPagination(@Arg("input") ticketInput: ListTicketsWithPagination): Promise<Ticket[]> {
+    return listTicketsWithPagination(ticketInput);
+  }
+
+  @Mutation(() => Ticket)
+  public async addTicket(@Arg("input") ticketInput: AddTicketInput): Promise<Ticket> {
+    return addTicket(ticketInput);
+  }
+
+  @Mutation(() => [Ticket])
+  public async saveTickets(@Arg("input") ticketInput: SaveTicketsInput): Promise<Ticket[]> {
+    return saveTickets(ticketInput);
+  }
+}
